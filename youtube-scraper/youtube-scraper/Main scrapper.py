b0f74fb51e7c588a746d72071abbde0dd349c1ee
Andrey Endovitskiy office <aendov56@gmail.com>
2024-09-10 16:30:58 +0700

    Adding youtube parcer

--- youtube-scraper/youtube-scraper/Main scrapper.py
+++ youtube-scraper/youtube-scraper/Main scrapper.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common import TimeoutException
 
 # initialize a web driver instance to control a Chrome window
 # in headless mode
@@ -14,6 +18,27 @@ driver = webdriver.Chrome(
 )
 
 # scraping logic...
+# the URL of the target page
+url = 'https://www.youtube.com/watch?v=kuDuJWvho7Q'
+# visit the target page in the controlled browser
+driver.get(url)
+
+try:
+    # wait up to 15 seconds for the consent dialog to show up
+    consent_overlay = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.ID, 'dialog'))
+    )
+
+    # select the consent option buttons
+    consent_buttons = consent_overlay.find_elements(By.CSS_SELECTOR, '.eom-buttons button.yt-spec-button-shape-next')
+    if len(consent_buttons) > 1:
+        # retrieve and click the 'Accept all' button
+        accept_all_button = consent_buttons[1]
+        accept_all_button.click()
+except TimeoutException:
+    print('Cookie modal missing')
+
+
 
 # close the browser and free up the resources
 driver.quit()
